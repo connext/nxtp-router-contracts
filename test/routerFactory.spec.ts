46e854db0aa8dba496e1bdf2929bfcbf7bbbefc2
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-25 00:20:04 +0530

    update test

--- test/routerFactory.spec.ts
+++ test/routerFactory.spec.ts
@@ -8,17 +8,8 @@ import { hexlify, keccak256, randomBytes } from "ethers/lib/utils";
 import { Wallet, BigNumber, BigNumberish, constants, Contract, providers, ContractReceipt } from "ethers";
 import { RevertableERC20, TransactionManager, FeeERC20, ERC20 } from "@connext/nxtp-contracts";
 import TransactionManagerArtifact from "@connext/nxtp-contracts/artifacts/contracts/TransactionManager.sol/TransactionManager.json";
-import RevertableERC20Artifact from "@connext/nxtp-contracts/artifacts/contracts/test/RevertableERC20.sol/RevertableERC20.json";
 
-import { InvariantTransactionData, VariantTransactionData } from "@connext/nxtp-utils";
-import {
-  deployContract,
-  MAX_FEE_PER_GAS,
-  convertToPrepareArgs,
-  signRemoveLiquidityTransactionPayload,
-  signPrepareTransactionPayload,
-  encodePrepareData,
-} from "./utils";
+import { deployContract } from "./utils";
 
 // import types
 import { Router, RouterFactory } from "../typechain-types";
@@ -30,7 +21,6 @@ const EmptyCallDataHash = keccak256(EmptyBytes);
 const createFixtureLoader = waffle.createFixtureLoader;
 describe("Router Contract", function () {
   const [wallet, router, routerReceipient, user, receiver, gelato, other] = waffle.provider.getWallets() as Wallet[];
-
   let routerFactory: RouterFactory;
   let transactionManagerReceiverSide: TransactionManager;
   let token: RevertableERC20;
@@ -43,35 +33,12 @@ describe("Router Contract", function () {
       receivingChainId,
     );
 
-    routerFactory = await deployContract<Router>("RouterFactory", transactionManagerReceiverSide.address);
+    routerFactory = await deployContract<RouterFactory>("RouterFactory", transactionManagerReceiverSide.address);
 
     return {
       routerFactory,
       transactionManagerReceiverSide,
     };
-  };
-
-  const addPrivileges = async (tm: TransactionManager, routers: string[], assets: string[]) => {
-    for (const router of routers) {
-      const tx = await tm.addRouter(router, { maxFeePerGas: MAX_FEE_PER_GAS });
-      await tx.wait();
-      expect(await tm.approvedRouters(router)).to.be.true;
-    }
-
-    for (const assetId of assets) {
-      const tx = await tm.addAssetId(assetId, {
-        maxFeePerGas: MAX_FEE_PER_GAS,
-      });
-      await tx.wait();
-      expect(await tm.approvedAssets(assetId)).to.be.true;
-    }
-  };
-
-  const approveTokens = async (amount: BigNumberish, approver: Wallet, spender: string, asset: ERC20 = token) => {
-    const approveTx = await asset.connect(approver).approve(spender, amount);
-    await approveTx.wait();
-    const allowance = await asset.allowance(approver.address, spender);
-    expect(allowance).to.be.at.least(amount);
   };
 
   let loadFixture: ReturnType<typeof createFixtureLoader>;
@@ -80,73 +47,16 @@ describe("Router Contract", function () {
   });
 
   beforeEach(async function () {
-    ({ transactionManagerReceiverSide, token } = await loadFixture(fixture));
-
-    // Prep contracts with router and assets
-    await addPrivileges(transactionManagerReceiverSide, [routerContract.address], [AddressZero, token.address]);
-
-    const liq = "10000";
-    let tx = await token.connect(wallet).transfer(router.address, liq);
-    await tx.wait();
-
-    await approveTokens(liq, router, transactionManagerReceiverSide.address);
-    let addLiquidityTx = await transactionManagerReceiverSide
-      .connect(router)
-      .addLiquidityFor(liq, token.address, routerContract.address);
-    await addLiquidityTx.wait();
+    ({ transactionManagerReceiverSide, routerFactory } = await loadFixture(fixture));
   });
-
-  const getTransactionData = async (
-    txOverrides: Partial<InvariantTransactionData> = {},
-    recordOverrides: Partial<VariantTransactionData> = {},
-  ): Promise<{
-    transaction: InvariantTransactionData;
-    record: VariantTransactionData;
-  }> => {
-    const transaction = {
-      receivingChainTxManagerAddress: transactionManagerReceiverSide.address,
-      user: user.address,
-      router: routerContract.address,
-      initiator: user.address,
-      sendingAssetId: AddressZero,
-      receivingAssetId: token.address,
-      sendingChainFallback: user.address,
-      callTo: AddressZero,
-      receivingAddress: receiver.address,
-      callDataHash: EmptyCallDataHash,
-      transactionId: hexlify(randomBytes(32)),
-      sendingChainId: sendingChainId,
-      receivingChainId: receivingChainId,
-      ...txOverrides,
-    };
-
-    const day = 24 * 60 * 60;
-    const block = await ethers.provider.getBlock("latest");
-    const record = {
-      amount: "10",
-      expiry: block.timestamp + day + 5_000,
-      preparedBlockNumber: 10,
-      ...recordOverrides,
-    };
-
-    return { transaction, record };
-  };
 
   describe("constructor", async () => {
     it("should deploy", async () => {
-      expect(routerContract.address).to.be.a("string");
+      expect(routerFactory.address).to.be.a("string");
     });
 
     it("should set transactionManagerAddress", async () => {
-      expect(await routerContract.transactionManager()).to.eq(transactionManagerReceiverSide.address);
-    });
-
-    it("should set routerSigner", async () => {
-      expect(await routerContract.routerSigner()).to.eq(router.address);
-    });
-
-    it("should set recipient", async () => {
-      expect(await routerContract.recipient()).to.eq(routerReceipient.address);
+      expect(await routerFactory.transactionManager()).to.eq(transactionManagerReceiverSide.address);
     });
   });
 });
