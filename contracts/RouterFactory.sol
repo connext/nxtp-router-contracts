b219b8ccc21529e43547388c53634e9129c29e93
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-25 00:14:39 +0530

    add ownable initiate with transactionManager remove addLiqudity func

--- contracts/RouterFactory.sol
+++ contracts/RouterFactory.sol
@@ -2,32 +2,25 @@
 pragma solidity 0.8.4;
 
 import "./interfaces/IRouterFactory.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@connext/nxtp-contracts/contracts/interfaces/ITransactionManager.sol";
 import "./Router.sol";
 
-contract RouterFactory is IRouterFactory {
+contract RouterFactory is IRouterFactory, Ownable {
 
   ITransactionManager public transactionManager;
 
-  constructor() {}
+  constructor(address _transactionManager) {
+    transactionManager = ITransactionManager(_transactionManager);
+  }
 
-  function init(address _transactionManager) public {
+  function setTransactionManager(address _transactionManager) external onlyOwner {
     transactionManager = ITransactionManager(_transactionManager);
   }
 
   function createRouter(address signer, address recipient) override external returns (address) {
     Router router = new Router(address(transactionManager), signer, recipient, msg.sender);
     emit RouterCreated(address(router));
-    return address(router);
-  }
-
-  function createRouterAndAddLiquidity(address signer, address recipient, address assetId, uint256 amount, bytes calldata signature) external payable returns (address) {
-    Router router = Router(this.createRouter(signer, recipient));
-    router.addLiquidity{value: LibAsset.isNativeAsset(assetId) ? amount : 0 }(
-      amount,
-      assetId,
-      signature
-    );
     return address(router);
   }
 }
